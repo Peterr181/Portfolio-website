3e96055e6bbdde78d3221d3dc7488b539626722b
Peterr181 <piotrzerdzinski089@gmail.com>
2023-01-06 14:22:51 +0100

    comm

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const revealSection = function (entries, observer) {
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.2,
+  threshold: 0.1,
 });
 
 allSections.forEach(function (section) {
@@ -46,8 +46,72 @@ navLink.forEach((link) => {
   });
 });
 
+console.log(navLink.length);
+
+// navLink.forEach((link) => {
+//   link.addEventListener("mouseenter", (e) => {
+//     console.log("chuj");
+//     e.target.style.color = "red";
+
+//     console.log(notTarget);
+//   });
+//   link.addEventListener("mouseleave", (e) => {
+//     console.log("chuj");
+//     e.target.style.color = "#fff";
+//   });
+// });
+// let current;
+
+// for (let i = 0; i < navLink.length; i++) {
+//   navLink[i].addEventListener("mouseenter", (e) => {
+//     navLink[i].style.color = "red";
+
+//     let nextSibling = navLink[i].nextElementSibling.textContent;
+//     console.log(nextSibling);
+//   });
+// }
+
+// for (let i = 0; i < navLink.length; i++) {
+//   navLink[i].addEventListener("mouseleave", (e) => {
+//     if (navLink[i]) {
+//     } else {
+//     }
+//   });
+// }
+
 // Making year updated
 const footering = document.querySelector(".copy");
 const currentYear = new Date().getFullYear();
 
 footering.innerHTML = `© Copyright ${currentYear}. Made by Peter Żerdziński`;
+
+// Switching images
+
+const backgroundImage = document.querySelector(".hihi");
+const items = document.querySelectorAll(".project__item");
+const images = document.querySelectorAll(".project__img");
+
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+
+let currentItemIndex = 0;
+
+prevButton.addEventListener("click", () => {
+  items[currentItemIndex].classList.remove("active");
+  currentItemIndex = (currentItemIndex - 1 + items.length) % items.length;
+  items[currentItemIndex].classList.add("active");
+  backgroundImage.setAttribute(
+    "src",
+    images[currentItemIndex].getAttribute("src")
+  );
+});
+
+nextButton.addEventListener("click", () => {
+  items[currentItemIndex].classList.remove("active");
+  currentItemIndex = (currentItemIndex + 1) % items.length;
+  items[currentItemIndex].classList.add("active");
+  backgroundImage.setAttribute(
+    "src",
+    images[currentItemIndex].getAttribute("src")
+  );
+});
